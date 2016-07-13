43c6e363275c864fab853f6c3e109a8f2963a1bd
Christopher Pourier <corez92@gmail.com>
2016-07-13 02:17:28 +0000

    language_detector/main.py

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -3,7 +3,23 @@
 LANGUAGES = [
     {
         'name': 'Spanish',
-        'common_words': ['yo', 'tu', 'el', 'nosotros']  # add your words here
+        'common_words': ['el', 'la', 'tu', 'yo', 'nosotros', 'quiero','bueno',
+        'gracias','grande','nada','si','por','Espana','hasta','usted','ud',
+        'ustedes','nadar','bien','llama','noches','escuela','economia',
+        'de','donde','tiene','entonces']  # add your words here
     },
-    #  include your extra languages here
+    {
+        'name': 'German',
+        'common_words': ['das', 'der', 'die', 'ja', 'bitte', 'nein', 'danke',
+        'hallo', 'gute', 'guten', 'gut', 'ist', 'du', 'ich', 'nicht', 'die',
+        'und', 'sie', 'der', 'was', 'wir', 'zu', 'ein', 'mit', 'mir', 'auf',
+        'mich', 'eine', 'wenn', 'haben']
+    },
+    {
+        'name': 'English',
+        'common_words': ['the', 'he', 'what', 'how', 'our', 'not', 'be',
+        'of', 'and', 'for', 'have', 'as', 'you', 'with', 'do', 'at', 'but',
+        'however', 'from', 'he', 'her', 'they', 'say', 'one', 'would', 'what',
+        'so', 'up', 'out', 'about']
+    }
 ]
--- language_detector/main.py
+++ language_detector/main.py
@@ -1,9 +1,30 @@
 """This is the entry point of the program."""
 
-from .languages import LANGUAGES
+from languages import LANGUAGES
 
 
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
+    text_lang = "Unknown"
     # implement your solution here
-    pass
+    a_list = text.split()
+    length_english = 0
+    try:
+        eng = [x for x in a_list for y in languages[2]['common_words'] if x==y]
+        length_english = len(eng)
+    except IndexError:
+        pass
+    ger = [x for x in a_list for y in languages[1]['common_words'] if x==y]
+    length_german = len(ger)
+    spa = [x for x in a_list for y in languages[0]['common_words'] if x==y]
+    length_spanish = len(spa)
+    print length_german, length_spanish
+    #if x>y and x>z:
+    #    text_lang = "English"
+    if length_german>length_spanish and length_german>length_english:
+        text_lang = "German"
+    elif length_english>length_spanish and length_english>length_german:
+        text_lang = "Englih"
+    else:
+        text_lang = "Spanish"
+    return text_lang
